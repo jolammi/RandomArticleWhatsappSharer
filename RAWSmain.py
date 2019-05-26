5df893d6b54550d9ac46db45bea0c6cc7e19f01e
Jouni Lammi <jjj.lammi@gmail.com>
2019-05-26 03:17:58 +0300

    First commit
    Working prototype

---
+++ RAWSmain.py
@@ -0,0 +1,119 @@
+#!/usr/local/bin/python3
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import requests
+import html2text
+
+
+#Control variables---------------------------------------------------------------------------------------------
+
+SELECTED_CONVERSATION = "Ryhmä" #ensure that this matches the contact or group name
+PATH_TO_CHROMEDRIVER = 'C:\chromedriver_win32\chromedriver.exe'
+SELECTED_LANGUAGE = "en" #this shows in wikipedia article and in the payload layout, currently "fi" or "en"
+QR_CODE_PERIOD_SECONDS = 30
+SLEEP_BETWEEN_MESSAGES = 600 #once in ten minutes
+
+#/Control variables--------------------------------------------------------------------------------------------
+
+def initialize_html2text():
+    h = html2text.HTML2Text()
+    h.ignore_links = True
+    return h
+
+def initialize_chromedriver(path):
+    driver = webdriver.Chrome(path)
+    return driver
+
+
+def print_help():
+    """Remember to install the correct chromedriver version from
+    http://chromedriver.chromium.org/downloads
+    For the app to work, you need to give it access to your whatsapp web by scanning the
+    QR code in the beginning. This only needs to be done once while the app is running. There
+    is a 30-second period to scan the QR code, after which the app will start to look for the contact.
+    - Set SELECTED_CONVERSATION to desired contact or group
+    - Set PATH_TO_CHROMEDRIVER to the correct path"""
+    return None
+
+
+def get_random_article(SELECTED_LANGUAGE):
+    page = requests.get('https://'+ SELECTED_LANGUAGE +'.wikipedia.org/wiki/Special:Random')
+    page_text = page.text
+
+    first_paragraph = str(page_text)
+    first_paragraph = first_paragraph[first_paragraph.find("<p>") +3 :]
+    first_paragraph = first_paragraph[: first_paragraph.find("</p>")]
+
+    page_link = str(page_text)
+    page_link = page_text[page_text.find("canonical"):]
+    page_link = page_link[page_link.find("=")+2 : page_link.find("/>")-1]
+
+    with open ("output.txt", "w") as output_file:
+        output_file.write(first_paragraph)
+    with open ("link.txt", "w") as output_file:
+        output_file.write(page_link)
+    return first_paragraph, page_link
+
+
+def parse_first_paragraph(text, handler,driver):
+    text = handler.handle(text)
+    symbols_to_replace = ["**", "_", "\\"]
+    for i in symbols_to_replace:
+        while text.find(i) != -1:
+            text = text.replace(i,"")
+    while text.find("\n") != -1:
+            text = text.replace("\n"," ")
+    while text.find("[") != -1:
+        text = text[:text.find("[")] + text[text.find("[")+3:] #get rid of references like [1]
+    return text
+
+def open_whatsapp_web(QR_CODE_PERIOD_SECONDS,driver):
+    driver.get('https://web.whatsapp.com')
+    time.sleep(QR_CODE_PERIOD_SECONDS)
+    return None
+
+def send_whatsapp_message(conversation, driver, text, link, message_box):
+    if SELECTED_LANGUAGE == "fi":
+        message_box.send_keys("Tiesitkö, että:")
+        ActionChains(driver).key_down(Keys.SHIFT).key_down(Keys.ENTER).key_up(Keys.ENTER).key_up(Keys.SHIFT).perform()
+        message_box.send_keys(text)
+        ActionChains(driver).key_down(Keys.SHIFT).key_down(Keys.ENTER).key_up(Keys.ENTER).key_up(Keys.SHIFT).perform()
+        message_box.send_keys("Kiinnostuitko? Katso lisää: ")
+        message_box.send_keys(link + "\n")
+    else:
+        message_box.send_keys("Did you know that:")
+        ActionChains(driver).key_down(Keys.SHIFT).key_down(Keys.ENTER).key_up(Keys.ENTER).key_up(Keys.SHIFT).perform()
+        message_box.send_keys(text)
+        ActionChains(driver).key_down(Keys.SHIFT).key_down(Keys.ENTER).key_up(Keys.ENTER).key_up(Keys.SHIFT).perform()
+        message_box.send_keys("Interested? Check: ")
+        message_box.send_keys(link + "\n")
+    return None
+
+def open_conversation(conversation,driver):
+    search_box = driver.find_element_by_xpath('//*[@title="Search or start new chat"]')
+    search_box.click()
+    search_box.send_keys(conversation + "\n")
+    message_box = driver.find_element_by_css_selector('#main > footer > div._3pkkz.copyable-area > div._1Plpp > div')
+    return message_box
+
+def main_loop(SELECTED_LANGUAGE,SELECTED_CONVERSATION):
+    print_help()
+    driver = initialize_chromedriver(PATH_TO_CHROMEDRIVER)
+    open_whatsapp_web(QR_CODE_PERIOD_SECONDS,driver)
+    message_box = open_conversation(SELECTED_CONVERSATION,driver)
+    handler = initialize_html2text()
+
+    while True:
+        try:
+            text, link = get_random_article(SELECTED_LANGUAGE)
+        except UnicodeEncodeError:
+            print("Unicode error")
+            continue
+        text = parse_first_paragraph(text, handler,driver) 
+        send_whatsapp_message(SELECTED_CONVERSATION,driver,text,link, message_box)
+        time.sleep(SLEEP_BETWEEN_MESSAGES)
+        # driver.quit()
+
+main_loop(SELECTED_LANGUAGE, SELECTED_CONVERSATION)
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# RandomArticleWhatsappSharer
+# RandomArticleWhatsappSharer (RAWS)
+RAWS is a Python3 app that can be used to share random Wikipedia knowledge to a Whatsapp group or a contact periodically.
+It's great for sharing a fact of the minute/hour/day for your friend(s).
+
+### What it does:
+  - Uses Chromedriver to drive WhatsApp web
+  - Fetches a random Wikipedia article in either Finnish or English, parses the first paragraph from it and turn the HTML version to plain text with html2text python library
+  - Fetches the contact or group by given keyword and sends the first paragraph with a link to the specified user
+  - Repeats the action when desired, max speed ~10 articles / minute
+  ---
+### How to use it
+- Install Chromedriver to your chrome version from http://chromedriver.chromium.org/downloads
+- Install the needed python packages:
+```sh
+pip3 install selenium
+pip3 install requests
+pip3 install html2text
+```
+- Set up control variables, they are located in the beginning of the python code
+- get your phone ready for scanning the Whatsapp QR code and start the app
+---
+### What it isn't
+- RAWS is at a "working prototype" stage. This means, that bugs are possible.
+- Known bugs:
+-- wrongly-written Wikipedia pages are likely to get printed wrongly
+-- "meta" pages such as category pages don't always work
+- The app isn't totally command-line because the whatsapp verification needs to be done via the GUI
+### What it is
+- A great way to either annoy or cheer up your friends
+- A great way to learn something new every day
+- A good start to develop something else to be shared via WhatsApp using Python
+  
+  
+  
+  
+
+
